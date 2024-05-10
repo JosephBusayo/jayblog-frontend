6eaa3c2b389303d8122b00a1748183b237033c29
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-05-10 07:14:08 +0100

    fixed proxy

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
+        target: "http://localhost:5000",
         base: "./",
       },
     },
